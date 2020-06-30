b9f4e2ce59054c24b7f706cbbcd090d9c64d9d0d
Bryan Ruminot S <bryan.ruminot@patagonia-it.com>
2020-06-30 00:26:09 -0400

    ADD: Get one item from book store

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -12,9 +12,10 @@ class BooksSpider(scrapy.Spider):
     def parse(self, response):
         for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
+            break
+        """ next_page = response.css("li.next > a ::attr(href)").extract_first()
         if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+            yield scrapy.Request(response.urljoin(next_page), callback=self.parse) """
 
     def parse_book_page(self, response):
         item = {}
